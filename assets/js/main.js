3fbca99b027d537f2f1e25b56dc9b6dc9b5a9d80
zshMVP <647255547@qq.com>
2022-07-31 21:54:00 +0800

    尝试添加加载遮罩

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
-console.warn("I Love SakuraJima Mai")
+console.warn("I Love SakuraJima Mai");
 console.log("%c Copyright %c zshMVP ","background: #606050;color: #fff;border-radius: 3px 0 0 3px","background: #1475B2;color: #fff;border-radius: 0 3px 3px 0");
+const LoadingHtml = "<div id='loading'><h2 data-text='SAKURAJIMA'>SAKURAJIMA</h2></div>";
+document.write(LoadingHtml)
+document.onreadystatechange = completeload;
+function completeload() {
+    if (document.readyState === "complete"){
+        let LoadingHtml = document.getElementById("loading")
+        LoadingHtml.parentElement.removeChild(LoadingHtml)
+    }
+
+}
+
