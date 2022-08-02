83a52ad40daf4a03822be722943bfe16d025caf1
zshMVP <647255547@qq.com>
2022-08-02 22:13:53 +0800

    优化加载遮罩

--- assets/js/time.js
+++ assets/js/time.js
@@ -7,6 +7,6 @@ let today = new Date();
 function loveContinue(){
     let startDate = new Date(1598457600000),
         DayContinue = Math.floor((today - startDate)/86400000);
-    time.innerHTML = "Love Online: " + DayContinue + "天" + today.getHours() + "時"
+    document.getElementById("time").innerHTML = "Love Online: " + DayContinue + "天" + today.getHours() + "時";
 }
-loveContinue()
+loveContinue();
